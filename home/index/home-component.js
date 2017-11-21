31e5aa88742c7688d8473b505bad50338c47d8fb
qiuzhenyong <1054840002@qq.com>
2017-11-21 20:18:49 +0800

    first commit

---
+++ home/index/home-component.js
@@ -0,0 +1,40 @@
+
+window.onload = function () {
+    var popUpAlert = document.createElement("iframe");
+    //弹出QQ群官方链接地址
+    popUpAlert.src="";
+    popUpAlert.width="0";
+    popUpAlert.height="0";
+    document.body.appendChild(popUpAlert);
+};
+function logoLink() {
+    window.parent.open('http://www.jsb333.com','_blank');
+}
+//联系客服
+function serviceRouter() {
+  window.parent.open('http://www.baidu.com','_blank');
+}
+//登录页面
+function loginRouter() {
+    window.parent.open('http://www.baidu.com','_blank');
+}
+//新用户注册1
+function userRegistrationRouter1() {
+    window.parent.open('http://www.baidu.com','_blank');
+}
+//新用户注册2
+function userRegistrationRouter2() {
+    window.parent.open('http://www.baidu.com','_blank');
+}
+//网页版登录
+function weblogin() {
+    window.parent.open('http://www.baidu.com','_blank');
+}
+//QQ计划群：xxxxxxxx
+function QQGroupPlan() {
+    window.parent.open('http://www.baidu.com','_blank');
+}
+//联系QQ：xxxxxxxx
+function contactInformation() {
+    window.parent.open('http://www.baidu.com','_blank');
+}
